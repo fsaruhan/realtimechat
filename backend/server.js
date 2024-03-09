a521ea9f48a6c47961d15e254b4a63e62f921c1e
fsaruhan <afatihsaruhan@gmail.com>
2024-03-09 16:28:24 +0200

    nodemon installation and testing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
 const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => console.log("Server is running on port 5000"));
+
+app.get("/", (req, res) => {
+  res.send("Hello dd");
+});
